4a99c5089abb424563e0a5e4056dfbe926667ebb
James Chen <ashchan@gmail.com>
2009-10-02 15:56:51 +0800

    added example for class ancestors

---
+++ 0-ancestors.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+class C
+end
+
+p C.ancestors                     # => [C, Object, Kernal, BasicObject]
+                                  # => [C, Object, Kernel] in Ruby 1.8
+
+p Class.ancestors                 # =>  [Class, Module, Object, Kernel, BasicObject]
+                                  # =>  [Class, Module, Object, Kernel] in Ruby 1.8
